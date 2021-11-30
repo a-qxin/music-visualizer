6386566cb14baa81944ff93d7f6efc14a48fa1a2
TonyT415 <72632848+TonyT415@users.noreply.github.com>
2021-11-29 20:53:48 -0800

    pushed what I have so far

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -3,7 +3,9 @@ import { List, Map } from 'immutable';
 
 // project dependencies
 import { PianoInstrument } from './instruments/Piano';
+import {PianoInstrument2} from './instruments/tonyt415';
 import { WaveformVisualizer } from './visualizers/Waveform';
+import { WaveformVisualizer2 } from './visualizers/tonyt415vis';
 
 /** ------------------------------------------------------------------------ **
  * The entire application state is stored in AppState.
@@ -17,8 +19,8 @@ import { WaveformVisualizer } from './visualizers/Waveform';
  */
 export type AppState = Map<string, any>;
 
-const instruments = List([PianoInstrument]);
-const visualizers = List([WaveformVisualizer]);
+const instruments = List([PianoInstrument,PianoInstrument2]);
+const visualizers = List([WaveformVisualizer, WaveformVisualizer2]);
 export const defaultState: AppState = Map<string, any>({
   instruments,
   visualizers,
---
+++ client/src/visualizers/tonyt415vis.tsx
@@ -0,0 +1,46 @@
+// 3rd party library imports
+import P5 from 'p5';
+import * as Tone from 'tone';
+
+// project imports
+import { Visualizer } from '../Visualizers';
+
+
+export const WaveformVisualizer2 = new Visualizer(
+  'Bar',
+  (p5: P5, analyzer: Tone.Analyser) => {
+    //these values are for findin size of the availible window
+    const width = window.innerWidth;
+    const height = window.innerHeight / 2;
+    const dim = Math.min(width, height);
+
+    p5.background(0, 0, 0, 255);
+    //thickness of lines
+    p5.strokeWeight(dim * 0.01);
+
+    //this is the color of the line/shape
+    p5.stroke(255, 0, 255, 255);
+    //if noFill(), then it wont "fill in the space between the middle and the lines"
+    p5.fill(0, 0, 255, 255);
+
+    const values = analyzer.getValue();
+    //parameter of begin shape affects the contents of the lines
+    // p5.beginShape();
+    // for (let i = 0; i < values.length; i++) {
+    //   const amplitude = values[i] as number;
+    //   const x = p5.map(i, 0, values.length - 1, 0, width);
+    //   const y = height / 2 + amplitude * height;
+    //   // Place vertex
+    //   p5.vertex(x, y);
+    // }
+    // p5.endShape();
+    for (let i = 0; i < values.length; i++) {
+        const amplitude = values[i] as number;
+        const x = p5.map(i, 0, values.length - 1, 0, width);
+        const y = 0 + 4 * (amplitude * height);
+        //const y = height / 2 + amplitude * height;
+        // Place vertex
+        p5.ellipse(x,height/2,1,y/2);
+      }
+  },
+);
