8084bc21c83a8e9ad52fc5e922936ee29602dbea
benthebenguin <bau2@mail.sfsu.edu>
2021-12-05 03:01:10 -0800

    modified schema file

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -15,15 +15,6 @@ CREATE TABLE songs (
 	release_date text NOT NULL 
 );
 
-DROP TABLE IF EXISTS songs;
-
-CREATE TABLE playlists (
-	id int NOT NULL PRIMARY KEY,
-	playlists_title text NOT NULL,
-	song_count int NOT NULL,
-	playlist_creator text NOT NULL
-);
-
 INSERT INTO songs (id, song_title, notes, artist, album, release_date) 
 VALUES (1, 'Ode to Joy (Dubstep Remix)', 'E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4', 'Beethoven', 'Mixtape', 'yes');
 
